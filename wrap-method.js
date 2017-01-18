c4460d80e2b6ae4a40fb6976baaae326872d8207
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-01-18 15:52:15 +0300

    Wrap-class example + cleanup wrap-method

--- wrap-method.js
+++ wrap-method.js
@@ -3,11 +3,6 @@ class Employee {
         this._hourlyPayRateUsd = 35;
     }
 
-    doLoggedPayment(timeEntries, paymentService) {
-        this.logPayment();
-        this.pay(timeEntries, paymentService);
-    }
-
     pay(timeEntries, paymentService) {
         var totalAmount = new Money();
 
@@ -17,47 +12,6 @@ class Employee {
         }
 
         paymentService.pay(this, totalAmount);
-    }
-
-    logPayment() {
-
-    }
-}
-
-class Employee {
-    constructor() {
-        this._hourlyPayRateUsd = 35;
-    }
-
-    pay(timeEntries, paymentService) {
-        this._logPayment();
-        this._doPay(timeEntries, paymentService);
-    }
-
-    _doPay(timeEntries, paymentService) {
-        var totalAmount = new Money();
-
-        for (var i = 0; i < timeEntries.length; i++) {
-            var timeEntry = timeEntries[i];
-            totalAmount.add(timeEntry.getHours() * this._hourlyPayRateUsd);
-        }
-
-        paymentService.pay(this, totalAmount);
-    }
-
-    _logPayment = function () {
-
-    }
-}
-
-class LoggingEmployee {
-    constructor(employee) {
-        this._employee = employee;
-    }
-
-    pay() {
-        this._logPayment();
-        this._employee.pay();
     }
 }
 
