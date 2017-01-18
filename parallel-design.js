7d2b76e51c6ac5046360cb0f9b3eee5c66f0bb0e
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-01-18 15:49:19 +0300

    Parallel design initial example.

---
+++ parallel-design.js
@@ -0,0 +1,14 @@
+class Contract {
+    constructor(data) {
+        this._data = data;
+    }
+
+
+    publish() {
+        // Some messy legacy, which is hard to test anf decouple.
+    }
+}
+
+class ContractNew {
+
+}
