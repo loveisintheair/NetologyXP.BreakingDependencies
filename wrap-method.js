b3fddb7d0c5fa396a457fd76e47cd1457fdbda52
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-01-18 14:59:46 +0300

    Wrap-method initial example.

---
+++ wrap-method.js
@@ -0,0 +1,21 @@
+class Employee {
+    constructor() {
+        this._hourlyPayRateUsd = 35;
+    }
+
+    pay(timeEntries, paymentService) {
+        var totalAmount = new Money();
+
+        for (var i = 0; i < timeEntries.length; i++) {
+            var timeEntry = timeEntries[i];
+            totalAmount.add(timeEntry.getHours() * this._hourlyPayRateUsd);
+        }
+
+        paymentService.pay(this, totalAmount);
+    }
+}
+
+class Money {
+
+}
+
